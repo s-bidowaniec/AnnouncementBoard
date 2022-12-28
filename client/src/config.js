865cce2fd31498a9648efcebfe331369d6c6ca26
Stanislaw Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2022-12-28 22:21:41 +0100

    Ad heroku api path

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,6 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:8000';
+export const API_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://kodilla-annoncements.herokuapp.com/'
+    : 'http://localhost:8000';
 export const IMGS_URL =
   process.env.NODE_ENV === 'production' ? '/uploads/' : 'http://localhost:8000/uploads/';
