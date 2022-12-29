55cd53e6cb49f7f448aeab65a2e03769f711840c
Stanislaw Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2022-12-29 12:03:34 +0100

    Fix production build, api path

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 export const API_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://kodilla-annoncements.herokuapp.com/'
-    : 'http://localhost:8000';
+  process.env.NODE_ENV === 'production' ? `${window.location.origin}` : 'http://localhost:8000';
 export const IMGS_URL =
-  process.env.NODE_ENV === 'production' ? '/uploads/' : 'http://localhost:8000/uploads/';
+  process.env.NODE_ENV === 'production'
+    ? `${window.location.origin}/uploads/`
+    : 'http://localhost:8000/uploads/';
