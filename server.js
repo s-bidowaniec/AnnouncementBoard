26b1b44b42c9a8b94bdb34eb01dd4aed8e795990
Stanislaw Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2022-12-29 13:11:54 +0100

    Add yarn engine

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 // Serve main react app
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 // Handle bad requests
