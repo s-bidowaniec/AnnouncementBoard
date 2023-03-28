08cb3ad5fb3095d10584be661416f19cf366439f
Stanislaw Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2023-03-28 19:18:08 +0200

    fix images

--- models/announcement.model.js
+++ models/announcement.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const announcementsSchema = new mongoose.Schema({
     title: {type: String, required: true, minLength: 10, maxLength: 50},
     content: {type: String, required: true, minLength: 20, maxLength: 1000},
-    date: {type: String, required: true},
+    date: {type: Date, required: true},
     photo: {type: String, required: true},
     price: {type: String, required: true},
     location: {type: String, required: true},
